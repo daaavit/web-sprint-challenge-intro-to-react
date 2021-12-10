042fe7de63a1ed7188bf8ca95fc1ae4a0950191e
Davit <daavit@yahoo.com>
2021-12-10 17:58:32 -0500

    finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,34 @@
 // Write your Character component here
 import React from "react";
-// import styled, from "styled-components";
+import styled from "styled-components";
+
+const CharWapper = styled.div`
+ .charNames{
+     display: flex;
+     flex-direction: row;
+     justify-content: space-evenly;
+     font-weight: bold;
+     color: black;
+     font-size: 1.5em;
+     letter-spacing: 2px;
+     padding: 30px;
+ }
+
+`
+
 
 const Character = (props) => {
     let star = props.star;
     console.log("Character props", star);
     return (
-        <div>
-            <span>{star.name}</span>
-            <span>{star.mass}</span>
-        </div>
+        <CharWapper>
+            <div class="charNames">
+                <span>{props.star.name}</span> <span>{star.birth_year}</span>
+            </div>
+            {/* <div >
+            <span>{star.birth_year}</span>
+            </div> */}
+        </CharWapper>
     );
 }
 
